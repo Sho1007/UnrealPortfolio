7bfa4b44ba388336ffd61bb23fcd693b4ce60c33
Sho1007 <sho1007@kookmin.ac.kr>
2023-04-03 11:26:51 +0900

    Modify Animation Resource

--- Unreal2304/Source/Unreal2304/Character/MyCharacter.h
+++ Unreal2304/Source/Unreal2304/Character/MyCharacter.h
@@ -26,4 +26,9 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	UPROPERTY(BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	bool bJumpButtonDown = false;
+	UPROPERTY(BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	bool bCrouchButtonDown = false;
 };
